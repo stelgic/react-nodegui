c705bd977dda7d502052b827a6ccf7696ec2f114
silvadom <dompayments@gmail.com>
2023-10-06 15:45:24 +0400

    add grid layout to splitter

--- src/components/Splitter/RNSpliter.ts
+++ src/components/Splitter/RNSpliter.ts
@@ -1,12 +1,13 @@
 import {
   QWidget,
   QSplitter,
-  Orientation,
-  QWheelEvent,
-  QSplitterSignals,
   QLayout,
   QBoxLayout,
-  QGridLayout
+  QGridLayout,
+  AlignmentFlag,
+  Orientation,
+  QObjectSignals,
+  QSplitterSignals
 } from "@nodegui/nodegui";
 import { setViewProps, ViewProps, SizePolicy } from "../View/RNView";
 import { throwUnsupported } from "../../utils/helpers";
@@ -21,6 +22,7 @@ export interface SplitterProps extends ViewProps<QSplitterSignals> {
   sizePolicy?: SizePolicy;
   orientation?: Orientation;
   layout?: QLayout | QBoxLayout | QGridLayout;
+  cols?: number;
 }
 
 const setSplitterProps = (
@@ -37,6 +39,9 @@ const setSplitterProps = (
     },
     set sizePolicy(policy: SizePolicy) {
       widget.setSizePolicy(policy.horizontal, policy.vertical);
+    },
+    set cols(count: number) {
+      widget.setProperty("cols", count);
     }
   };
   Object.assign(setter, newProps);
@@ -47,6 +52,20 @@ const setSplitterProps = (
  * @ignore
  */
 export class RNSplitter extends QSplitter implements RNWidget {
+  private _layout: QLayout<QObjectSignals> | null = null;
+  private _isGridlayout: boolean = false;
+  private _numChildrens: number = 0;
+
+  layout() {
+    return this._layout;
+  }
+  setLayout(layout: QLayout<QObjectSignals>) {
+    if(layout instanceof QGridLayout) {
+      this._isGridlayout = true;
+    } 
+    this._layout = layout;
+    super.setLayout(layout);
+  }
   setProps(newProps: SplitterProps, oldProps: SplitterProps): void {
     setSplitterProps(this, newProps, oldProps);
   }
@@ -55,7 +74,21 @@ export class RNSplitter extends QSplitter implements RNWidget {
   }
   appendInitialChild(child: QWidget<any>): void {
     if (child instanceof QWidget) {
-      this.addWidget(child);
+      if(this._isGridlayout) {
+        const cols = this.property("cols").toInt();
+        const grid = this.layout() as QGridLayout;
+        var num = this._numChildrens;
+        const row = Math.floor(num / cols);
+        const col = Math.max(0, (num % cols));
+        this._numChildrens += 1;
+        
+        (this.layout() as QGridLayout)!.addWidget(child, row, col, 1, 1,
+                                                AlignmentFlag.AlignLeft |
+                                                AlignmentFlag.AlignTop);
+                    
+      } else {
+        this.addWidget(child);
+      }
     } else {
       throwUnsupported(this);
     }
